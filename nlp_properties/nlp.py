e85af43ae7b6692582a4c758bb4ee84ffb7274f6
Rik Goldman <rikgoldman@gmail.com>
2018-05-11 06:02:11 -0400

    nlp-stub

---
+++ nlp_properties/nlp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import nltk
+
+class NLP(GetText):
+    
+    def pipeline(text):
+        self.sentences = nltk.sent_tokenize(text)
+        for sentence in self.sentences:
+            self.words = nltk.word_tokenize(sentence)
+            self.tagged_words = nltk.pos_tag(self.words)
+            self.ne_tagged_words = nltk.ne_chunk(self.tagged_words)
+            print ne_tagged_words
