55e3a1019861fad81f822693a1efa9e5dc414cab
Rik Goldman <rikgoldman@gmail.com>
2018-05-11 07:36:41 -0400

    working on NLP object

--- nlp_properties/nlp.py
+++ nlp_properties/nlp.py
@@ -2,12 +2,12 @@
 
 import nltk
 
-class NLP(GetText):
-    
-    def pipeline(text):
+class NLP():
+
+    def __init__(self, text):
         self.sentences = nltk.sent_tokenize(text)
         for sentence in self.sentences:
             self.words = nltk.word_tokenize(sentence)
             self.tagged_words = nltk.pos_tag(self.words)
             self.ne_tagged_words = nltk.ne_chunk(self.tagged_words)
-            print ne_tagged_words
+            print self.ne_tagged_words
